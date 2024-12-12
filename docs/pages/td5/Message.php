fe9bd67a3f36efda76d3dd6c203763443e86c603
Théo Fauvel <tfauvel@info.local>
2024-12-12 13:54:19 +0100

    MAJ de Message.php sur TD5

--- docs/pages/td5/Message.php
+++ docs/pages/td5/Message.php
@@ -1,19 +1,24 @@
 <?php
-// Vérifier si un message est passé dans l'URL via la méthode GET
-$message = isset($_GET['message']) ? $_GET['message'] : 'Message par défaut';
+// Vérifie si le paramètre 'message' est absent dans l'URL
+if (!isset($_GET['message']) || empty($_GET['message'])) {
+    // Redirige automatiquement vers l'URL avec le message par défaut
+    header("Location: ?message=Message%20par%20défaut");
+    exit(); // Arrête l'exécution après la redirection
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Message</title>
 </head>
 <body>
 
-        <li><a href="message.php?message=Bonjour%20le%20monde">Afficher "Bonjour le monde"</a></li>
-        <li><a href="message.php?message=Message%20important">Afficher "Message important"</a></li>
-        <li><a href="message.php?message=Bienvenue%20sur%20le%20site">Afficher "Bienvenue sur le site"</a></li>
+        <li><a href="message.php?message=Bonjour%20le%20monde"> Afficher Bonjour le monde</a></li>
+        <li><a href="message.php?message=Message%20important"> Afficher Message important</a></li>
+        <li><a href="message.php?message=Bienvenue%20sur%20le%20site"> Afficher Bienvenue sur le site</a></li>
 
 </body>
 </html>
+
+
